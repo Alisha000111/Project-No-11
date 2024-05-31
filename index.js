202004085ff3d63163f4f71a92423741e94960a7
Alisha <alishamirfan1@gmail.com>
2024-05-31 11:42:23 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,35 @@
+import inquirer from "inquirer";
+import { differenceInSeconds } from "date-fns";
+const res = await inquirer.prompt({
+    name: "userInput",
+    type: "number",
+    message: "Pleacse enter amount of second",
+    validate: (input) => {
+        if (isNaN(input)) {
+            return "please enter valid number";
+        }
+        else if (input > 60) {
+            return "seconds must within 60";
+        }
+        else {
+            return true;
+        }
+    }
+});
+let input = res.userInput;
+function startTime(val) {
+    const intTime = new Date().setSeconds(new Date().getSeconds() + val);
+    const intervalTime = new Date(intTime);
+    setInterval((() => {
+        const currentTime = new Date();
+        const timeDiff = differenceInSeconds(intervalTime, currentTime);
+        if (timeDiff <= 0) {
+            console.log("Timer has expired");
+            process.exit();
+        }
+        const min = Math.floor((timeDiff % (3600 * 24)) / 3600);
+        const sec = Math.floor(timeDiff % 60);
+        console.log(`${min.toString().padStart(2, "0")}:${sec.toString().padStart(2, "0")}`);
+    }), 1000);
+}
+startTime(input);
